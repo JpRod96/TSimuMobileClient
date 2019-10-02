5064c410bd11392a170251410b927a51d04d0fd7
JpRod96 <jprodriguez60@gmail.com>
2019-10-02 16:18:25 -0400

    Creado modelo Event

---
+++ src/model/event.ts
@@ -0,0 +1,20 @@
+export class Event {
+    id: string;
+    date: any;
+    delationDate: any;
+    description: string;
+    locationDescription: string;
+
+    constructor(id,
+                date,
+                delationDate,
+                description,
+                locationDescription)
+    {
+        this.id = id;
+        this.date = date;
+        this.delationDate = delationDate;
+        this.description = description;
+        this.locationDescription = locationDescription;
+    }
+}
